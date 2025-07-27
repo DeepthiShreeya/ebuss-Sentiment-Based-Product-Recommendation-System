d3a0b4d26b78d6e6066edec5b81e066394592bec
Deepthi Shreeya <105097563+DeepthiShreeya@users.noreply.github.com>
2025-07-27 14:50:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 RANDOM_STATE = 42
 DATA_PATH    = "data/sample30.csv"     
 AUG_PATH     = "data/augment_data.csv"
-OUTPUT_DIR   = "artifacts"
+OUTPUT_DIR   = "pickles"
 TOPK_CF      = 200   # neighbors for CF
 ALPHA_BETA_GAMMA = (0.6, 0.3, 0.1)       # hybrid weights for CF scores 
