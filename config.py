39f4e73012895766a0dd1399e6be2ae4ce601d78
Deepthi Shreeya <105097563+DeepthiShreeya@users.noreply.github.com>
2025-07-30 05:41:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PICKLE_DIR = os.path.join(BASE_DIR, "pickles")
 
+# Required by prepare_artifacts.py
+RANDOM_STATE     = 42
+DATA_PATH        = '/content/drive/MyDrive/EPGP in ML and AI/sample30.csv'
+AUG_PATH         = '/content/drive/MyDrive/EPGP in ML and AI/augment_data.csv'
+OUTPUT_DIR       = '/content/drive/MyDrive/EPGP in ML and AI/OUTPUT'
+TOPK_CF          = 20
+ALPHA_BETA_GAMMA = (0.6, 0.3, 0.1)
+
 class Config:
     USER_COL       = "reviews_username"
     PRODUCT_COL    = "name"
